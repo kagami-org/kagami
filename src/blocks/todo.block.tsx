ae31de8860d53c040ac6c8dbbcba1198cafae35a
Shanu Jha <shanu.j@foxberry.in>
2024-10-27 21:18:10 +0530

    feat(todo): Add functionality to remove tasks via a delete button in TaskComponent refactor: Update TodoBlock component for better readability and maintainability

--- src/blocks/todo.block.tsx
+++ src/blocks/todo.block.tsx
@@ -1,16 +1,15 @@
-import { FormEvent, useState } from "react"
+import { FormEvent, useState } from "react";
 
 type Task = {
-  id: string,
-  order: number,
-  name: string,
-  checked: boolean,
-}
+  id: string;
+  order: number;
+  name: string;
+  checked: boolean;
+};
 
 export const TodoBlock = () => {
-
-  const [tasks, setTasks] = useState<Task[]>([])
-  const [task, setTask] = useState<string>('')
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [task, setTask] = useState<string>("");
 
   function newTask(title: string): Task {
     return {
@@ -18,48 +17,111 @@ export const TodoBlock = () => {
       order: tasks.length,
       name: title,
       checked: false,
-    }
+    };
   }
 
   function handleSubmit(e: FormEvent) {
-    e.preventDefault()
-    if (task.trim() === '') return;
-    setTasks(prevTasks => [...prevTasks, newTask(task)]);
-    setTask('')
+    e.preventDefault();
+    if (task.trim() === "") return;
+    setTasks((prevTasks) => [...prevTasks, newTask(task)]);
+    setTask("");
   }
 
   function toggleCheckTask(theTask: Task) {
-    const filter = tasks.filter(task => task.id !== theTask.id)
+    const filter = tasks.filter((task) => task.id !== theTask.id);
     const update = theTask;
     update.checked = !update.checked;
     const copy = [...filter, update];
-    setTasks(copy)
+    setTasks(copy);
+  }
+
+  function removeTask(theTask: Task) {
+    const filter = tasks.filter((task) => task.id !== theTask.id);
+    setTasks(filter);
   }
 
   return (
     <div>
       <h1> My list </h1>
       <form onSubmit={handleSubmit}>
-        <input value={task} onChange={(e) => { setTask(e.target.value) }} type="text" placeholder="What's on your mind" />
+        <input
+          value={task}
+          onChange={(e) => {
+            setTask(e.target.value);
+          }}
+          type="text"
+          placeholder="What's on your mind"
+        />
       </form>
-      <ul style={{listStyle: 'none', padding: 0, margin: 0}}>
-        {tasks.sort((a, b) => a.order - b.order).map(task => (<TaskComponent onClick={() => toggleCheckTask(task)} key={task.id} task={task} />))}
-      </ul>
+      <div
+        style={{
+          listStyle: "none",
+          padding: 0,
+          margin: 0,
+          marginTop: 20,
+          display: "flex",
+          flexDirection: "column",
+          gap: 5,
+        }}
+      >
+        {tasks
+          .sort((a, b) => a.order - b.order)
+          .map((task) => (
+            <TaskComponent
+              onRemove={() => removeTask(task)}
+              onClick={() => toggleCheckTask(task)}
+              key={task.id}
+              task={task}
+            />
+          ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
 interface ITaskComponent {
-  task: Task,
-  onClick: Function,
+  task: Task;
+  onClick: Function;
+  onRemove: Function;
 }
 function TaskComponent(props: ITaskComponent) {
-  const { task, onClick } = props;
-  const { checked } = task
+  const { task, onClick, onRemove } = props;
+  const { checked } = task;
 
-  function click() { onClick() }
+  function click() {
+    onClick();
+  }
+  function remove() {
+    onRemove();
+  }
 
-  return <li onClick={click} style={{ maxWidth: 100, margin: 0, padding: 0, cursor: 'pointer', listStyle: 'none', color: checked ? 'gray' : 'black' }} key={task.id}>{checked ? "☑️ " : "⬜ "}{task.name}</li>
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "start",
+        gap: 10,
+      }}
+    >
+      <div
+        onClick={click}
+        style={{
+          minWidth: 125,
+          margin: 0,
+          padding: 0,
+          cursor: "pointer",
+          listStyle: "none",
+          color: checked ? "gray" : "black",
+        }}
+        key={task.id}
+      >
+        {checked ? "☑️ " : "⬜ "}
+        {task.name}
+      </div>
+      <button style={{ cursor: "pointer" }} onClick={remove}>
+        {" 🗑️"}
+      </button>
+    </div>
+  );
 }
-
-

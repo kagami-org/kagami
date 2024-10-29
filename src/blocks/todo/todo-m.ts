a93ff52c407e9f5cb88ae1ac63e14fea578b2efc
Shanu Jha <shanu.j@foxberry.in>
2024-10-29 11:28:13 +0530

    feat: Add id field to Task type and create Record type with id and tb fields.

--- src/blocks/todo/todo-m.ts
+++ src/blocks/todo/todo-m.ts
@@ -1,6 +1,15 @@
 export type Task = {
+  id?: Record;
   task: string;
   order: number;
   name: string;
   checked: boolean;
 };
+
+
+type Record = {
+  id: {
+    String: string
+  },
+  tb: string
+}
